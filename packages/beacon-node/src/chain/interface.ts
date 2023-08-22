e119753da0a3826f9dc3291ffc24869b25032175
matthewkeil <me@matthewkeil.com>
2023-08-22 07:50:23 -0400

    fix: update IBeaconChain and clear error in beaconBlocksByRoot

--- packages/beacon-node/src/chain/interface.ts
+++ packages/beacon-node/src/chain/interface.ts
@@ -143,6 +143,9 @@ export interface IBeaconChain {
     blockAttributes: BlockAttributes
   ): Promise<{block: allForks.BlindedBeaconBlock; executionPayloadValue: Wei}>;
 
+  blindedBlockToFull(block: allForks.FullOrBlindedSignedBeaconBlock): Promise<allForks.SignedBeaconBlock>;
+  blindedBlockToFullBytes(block: Uint8Array): Promise<Uint8Array>;
+
   /** Process a block until complete */
   processBlock(block: BlockInput, opts?: ImportBlockOpts): Promise<void>;
   /** Process a chain of blocks until complete */
